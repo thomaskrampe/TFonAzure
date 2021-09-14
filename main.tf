a137692dafdb12bdb51dae7dbfdb4efb966a036e
Thomas Krampe <thomas.krampe@myctx.net>
2021-09-14 12:29:19 +0200

    Add comments

--- main.tf
+++ main.tf
@@ -1,7 +1,10 @@
+# Configure the Azure Provider
+# <https://www.terraform.io/docs/providers/Getazurerm/index.html>
 provider "azurerm" {
    features {}
 }
 
+# Azure Remote Backend
 terraform {
   backend "azurerm" {
     resource_group_name  = "tfstate-rg"
@@ -11,7 +14,14 @@ terraform {
   }
 }
 
+# Create a resource group
+# <https://www.terraform.io/docs/providers/azurerm/r/resource_group.html>
 resource "azurerm_resource_group" "rg-ghactions-tf" {
-  name     = "${var.resource_group_name}"
-  location = "${var.azure_region}"
+  name     = var.resource_group_name
+  location = var.azure_region}
+  tags = {
+    "OwnerName" = "t.krampe@loginconsultants.de"
+    "Environment" = "Test"
+    "ApplicationName" = "TF-GHA-Azure"
+  }
 }
