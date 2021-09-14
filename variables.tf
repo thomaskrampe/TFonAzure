46a8624f2395d627f8f7bc34ac1fa5834713c42c
Thomas Krampe <thomas.krampe@myctx.net>
2021-09-14 13:14:41 +0200

    Initial

--- variables.tf
+++ variables.tf
@@ -1,11 +1,9 @@
 variable "resource_group_name" {
     type = string
     description = "Name of the resourece group"
-  
 }
 
 variable "azure_region" {
     type = string
     description = "Azure region"
-  
 }
